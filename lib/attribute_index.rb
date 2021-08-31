951966af8daf1f59ad893eb2804d3bb39d73e984
Hemanth <h.haridas@pks.com.au>
2021-09-01 08:51:53 +1000

    Remove commented code

--- lib/attribute_index.rb
+++ lib/attribute_index.rb
@@ -1,3 +1,4 @@
+# Stores
 class AttributeIndex
   attr_accessor :index_name, :attribute_name, :attr_index
 
@@ -12,14 +13,6 @@ class AttributeIndex
     attr_index[process(attr_val)] ||= []
     attr_index[process(attr_val)] << doc_index
 
-    # if attr_val.is_a?(Array)
-    #   index_array(attr_val, doc_index)
-    # elsif attr_val.is_a?(String)
-    #   index_str(attr_val, doc_index)
-    # else
-    #   index_other(attr_val, doc_index)
-    # end
-    #
   end
 
   def search(term)
