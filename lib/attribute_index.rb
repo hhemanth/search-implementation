cc0e2c8044dd7ec97c645c978d4d339bd2e68196
Hemanth <h.haridas@pks.com.au>
2021-09-01 18:58:00 +1000

    Added comments for AttributeIndex and SrcIndex classes

--- lib/attribute_index.rb
+++ lib/attribute_index.rb
@@ -1,4 +1,14 @@
-# Stores
+# Stores hash table where the key is a search term and value is the id of documents it is found in
+# Each of the attributes in a Document has an Attribute Index.
+# Consider the following Index (Document), index_name: Person
+# [{"_id" => 1, "name" => "Hemanth Haridas", "age"=> 35, "occupation"=> "Software Engineer"},
+#  {"_id" => 2, "name" => "Jack Straw", "age"=> 25, "occupation"=> "Senior Software Engineer"},
+# ]
+# Then the indexing will result in the following Attribute indexes
+# AttributeIndex index_name: Person, attribute_name: name, attr_index: {"hemanth" => [1], "haridas"=>[1], "jack"=>[2], "straw"=>[2]}
+# AttributeIndex index_name: Person, attribute_name: age, attr_index: {"35" => [1], "25"=>[2]}
+# AttributeIndex index_name: Person, attribute_name: occupation, attr_index: {"software" => [1,2], "engineer"=>[1,2], "senior"=> [2]}
+# AttributeIndex index_name: Person, attribute_name: id, attr_index: {"1" => [1], "2"=>[2]}
 class AttributeIndex
   attr_accessor :index_name, :attribute_name, :attr_index
 
--- lib/src_index.rb
+++ lib/src_index.rb
@@ -1,3 +1,14 @@
+# Stores hash table where the key is the ID of the document and value is the whole document
+
+# Consider the following Index (Document), index_name: Person
+# [{"_id" => 1, "name" => "Hemanth Haridas", "age"=> 35, "occupation"=> "Software Engineer"},
+#  {"_id" => 2, "name" => "Jack Straw", "age"=> 25, "occupation"=> "Senior Software Engineer"},
+# ]
+# Then the indexing will result in the following SrcIndex
+# SrcIndex index_name: Person,  source_index: {"1" => {"_id" => 1, "name" => "Hemanth Haridas", "age"=> 35, "occupation"=> "Software Engineer"},
+#                                              "2" => {"_id" => 2, "name" => "Jack Straw", "age"=> 25, "occupation"=> "Senior Software Engineer"}
+#                                              }
+
 class SrcIndex
   attr_accessor :index_name,:source_index
   def initialize(index_name)
